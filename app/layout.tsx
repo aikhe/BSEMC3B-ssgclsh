d3e557f947429ec1340389ac9041fa624a76bba7
Luoie8340 <itslukiee@gmail.com>
2024-05-13 03:08:52 +0800

    warning: holy fucking shitty html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Titan_One } from "next/font/google";
-import "./(landing)/styles/globals.scss";
-import { Header } from "@/components/landing/ui/header";
+import { DM_Sans } from "next/font/google";
+import "./(root)/styles/globals.scss";
 
-const titanOne = Titan_One({
-  weight: ["400"],
-  preload: false,
-});
+const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sausage Clash",
-  description: "A thrilling 3D dodgeball game",
+  title: "hjkl",
+  description: "The only Next-js template that will suite my need",
 };
 
 export default function RootLayout({
@@ -19,20 +15,50 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      // className="scroll-smooth"
-      lang="en"
-      data-theme="fantasy"
-      suppressHydrationWarning
-    >
+    <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
-        {/* <link href="https://fonts.cdnfonts.com/css/valorant" rel="stylesheet" /> */}
+        <link rel="icon" type="image/svg+xml" href="/icon.png" />
       </head>
-      <body className={`${titanOne.className}`}>
-        {/* <Header /> */}
-        {children}
-      </body>
+      <body className={dmSans.className}>{children}</body>
     </html>
   );
 }
+
+// import type { Metadata } from "next";
+// import { Titan_One } from "next/font/google";
+// import "./(landing)/styles/globals.scss";
+// // import { Header } from "@/components/landing/ui/header";
+
+// const titanOne = Titan_One({
+//   weight: ["400"],
+//   preload: false,
+// });
+
+// export const metadata: Metadata = {
+//   title: "Sausage Clash",
+//   description: "A thrilling 3D dodgeball game",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html
+//       // className="scroll-smooth"
+//       lang="en"
+//       data-theme="fantasy"
+//       suppressHydrationWarning
+//     >
+//       <head>
+//         <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+//         {/* <link href="https://fonts.cdnfonts.com/css/valorant" rel="stylesheet" /> */}
+//       </head>
+//       <body className={`${titanOne.className}`}>
+//         {/* <Header /> */}
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
